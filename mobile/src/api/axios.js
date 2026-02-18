c6005ec1ff93220d75bab4d4b40940e27c6c5ff6
VinayPal90 <118743523+VinayPal90@users.noreply.github.com>
2026-02-19 03:48:49 +0530

    Update BASE_URL for deployment to Render

--- mobile/src/api/axios.js
+++ mobile/src/api/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // LOCAL testing ke liye apna IPv4 daalo
 // DEPLOYMENT ke liye apni Render/Vercel link yahan daal dena
-const BASE_URL = 'http://192.168.1.41:5000/api'; 
+// const BASE_URL = 'http://192.168.1.41:5000/api';   
+const BASE_URL = 'https://marketplace-fullstack-backend.onrender.com/';
 // const BASE_URL = 'https://your-backend-api.onrender.com/api'; // Deployment ke waqt ise uncomment karna
 
 const API = axios.create({
